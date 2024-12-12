47bb5b483ebdda3d9e109b5aeea31fadf06bb8c7
samjoshuadud <calebjoshuaarmojallas@gmail.com>
2024-12-12 20:49:24 +0800

    Fix Users Date to Local TIme

---
+++ UniLostAndFound/UniLostAndFound.API/Migrations/20241212123545_AddCreatedAtDefaultValue.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UniLostAndFound.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreatedAtDefaultValue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "UserAccess",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValueSql: "CONVERT_TZ(CURRENT_TIMESTAMP(6), '+00:00', '+08:00')",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)",
+                oldDefaultValueSql: "CURRENT_TIMESTAMP(6)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "UserAccess",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP(6)",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)",
+                oldDefaultValueSql: "CONVERT_TZ(CURRENT_TIMESTAMP(6), '+00:00', '+08:00')");
+        }
+    }
+}
---
+++ UniLostAndFound/UniLostAndFound.API/Migrations/20241212123626_AddCreatedAtDefaultValueToManila.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UniLostAndFound.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreatedAtDefaultValueToManila : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "UserAccess",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP(6) AT TIME ZONE 'Asia/Manila'",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)",
+                oldDefaultValueSql: "CONVERT_TZ(CURRENT_TIMESTAMP(6), '+00:00', '+08:00')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "UserAccess",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValueSql: "CONVERT_TZ(CURRENT_TIMESTAMP(6), '+00:00', '+08:00')",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)",
+                oldDefaultValueSql: "CURRENT_TIMESTAMP(6) AT TIME ZONE 'Asia/Manila'");
+        }
+    }
+}
